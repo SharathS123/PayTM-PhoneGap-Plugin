f2a53f644352da1522e68915120deaccdea54b28
SharathS123 <sharath.s@hashedin.com>
2017-05-26 14:55:06 +0530

    Small changes

--- www/paytm.js
+++ www/paytm.js
@@ -1,5 +1,6 @@
 module.exports = {
   startPayment: function(orderId, customerId, email, phone, amount, successCallback, failureCallback) {
+    console.log("Function call happening");
     cordova.exec(successCallback,
                  failureCallback, 
                  "PayTM",
